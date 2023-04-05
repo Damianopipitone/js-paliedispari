fa5e56a82ffeac8c57e63832930f44f541db294e
Damianopipitone <damianopipitone33@gmail.com>
2023-04-05 16:20:38 +0200

    Palindromi

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,11 +1,22 @@
+// L'UTENTE INSERISCE UNA PAROLA
+
 let userWord = prompt("Inserisci una parola...");
 
+// ---- FUNZIONI -----
+
+// CREO UNA FUNZIONE PER VERIFICARE SE LA PAROLA DELL'UTENTE È PALINDROMA
 
 function validatePalindrome (palindromeWord) {
 
+    // CREO UNA VARIABILE CHE MI TENGA CONTO DELLA LUNGHEZZA DELLA PAROLA
+
     const word = (userWord.length);
 
+    // APRO UN CICLO CHE MI SPEZZA LA PAROLA IN DUE 
+
     for (let i = 0; i < word / 2; i++) {
+
+        // SE LE DUE METÀ CORRISPONDONO LA PAROLA È PALINDROMA
 
         if (userWord[i] !== userWord[word -1 -i]) {
 
